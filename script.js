80273c70527b0a5e0749bbdd01c54be9c3e71a22
Moringa School <samuel.kamotho@student.moringaschool.com>
2021-08-03 13:27:51 +0300

    it will link html with script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 function naming() {
+
     var CC = parseInt(document.getElementById("main").value);
     var YY = parseInt(document.getElementById("name").value);
     var MM = parseInt(document.getElementById("Form").value);
     var DD = parseInt(document.getElementById("akan").value);
 
-    var day = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sartuday");
-    var femaleNames = ("Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", " Afua", "Ama");
-    var maleNames = ("Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame");
     var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sartuday"];
     var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", " Afua", "Ama"];
     var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
@@ -14,6 +12,7 @@ function naming() {
     if (CC.value == "" || YY.value == "" || MM.value == "" || DD.value == "") {
         alert("No blank values allowed");
     }
+
     var gender = document.querySelector('input[name="gender"]:checked').value;
     var dayOfTheWeek = parseInt(((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
 
@@ -40,6 +39,7 @@ function naming() {
             alert("You were born on: " + day[6]);
             alert("Your Akan name is: " + maleNames[6]);
         }
+
     } else {
         if (dayOfTheWeek === 0) {
             alert("You were born on: " + day[0]);
